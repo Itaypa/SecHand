00a4d4a7cea0a724c68fe3aae495be568c5640cf
Pahima7 <pahima7@gmail.com>
2017-05-02 20:32:45 +0300

    initial maps support

--- SecHand/FindMatchPage.cs
+++ SecHand/FindMatchPage.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
 namespace SecHand
 {
 	public class FindMatchPage : ContentPage
 	{
 		public FindMatchPage()
 		{
-			Title = "Find a match";
+			var map = new Map(MapSpan.FromCenterAndRadius(
+			    new Position(37, -122), Distance.FromMiles(0.3)))
+			{
+				IsShowingUser = true,
+				HeightRequest = 100,
+				WidthRequest = 960,
+				VerticalOptions = LayoutOptions.FillAndExpand
+			};
+			var stack = new StackLayout { Spacing = 0 };
+			stack.Children.Add(map);
+			Content = stack;
 
 		}
 	}
